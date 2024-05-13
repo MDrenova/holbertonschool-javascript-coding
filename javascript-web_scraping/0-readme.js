6cb020cb588241469bbf84ffe0894dab3259eccf
root <mateodrenova@gmail.com>
2024-05-13 12:37:12 +0000

    Update: file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-const fs = require("fs");
-const fileName = process.arg[2];
-
-fs.readFile(fileName, { encoding: 'utf-8', flag: 'r' }, (error, fileContent) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    console.log(fileContent);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
